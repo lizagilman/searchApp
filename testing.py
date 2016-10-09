8e054d465b918755f518a7914a0010c9e20245dc
lizag <lizagilman@gmail.com>
2016-10-10 02:27:13 +0300

    get song plain text, get song name, get artist name, get list of all words of song

--- testing.py
+++ testing.py
@@ -8,14 +8,8 @@ import pprintpp
 
 
 def getSong(url, filename):
-
     html = urllib.urlopen(url)
     soup = BeautifulSoup(html, "lxml")
-
-
-
-
-
     for script in soup(["script", "style"]):
         script.extract()    # rip it out
     # get text
@@ -26,30 +20,53 @@ def getSong(url, filename):
     chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
     # drop blank lines
     text = '\n'.join(chunk for chunk in chunks if chunk)
-
-
     PRINT='Print'
     NEW_LINE = '\n'
     print_str_index = text.find(PRINT)
     text = text[print_str_index+len(PRINT)+1:]
-
-    for i in range(3):
-        newline_index = text.find(NEW_LINE)
-        text = text[newline_index+len(NEW_LINE):]
-
+    artist_start = text.find(NEW_LINE)
+    text = text[artist_start + len(NEW_LINE):]
+    artist_end = text.find(NEW_LINE)
+    artist = text[0:artist_end]
+    artist = artist.rpartition(' ')[0].lower()
+    song_name_start = text.find(NEW_LINE)
+    text = text[song_name_start+len(NEW_LINE):]
+    song_name_end = text.find(NEW_LINE)
+    song_name = text[1:song_name_end-1]
+    newline_index = text.find(NEW_LINE)
+    text = text[newline_index+len(NEW_LINE):]
     song_end = text.find("Submit Corrections\nVisit www.azlyrics.com for these lyrics.")
     text = text[:song_end]
+    text = text.replace(',','')
+    text = text.replace('?', '')
+    #TO-DO: remove all non-alphabetic characters, not only ',' and '?'
+
+    return {'song_text': text, 'artist': artist, 'text': text}
+
+# get all words without repetitions
+def get_words():
+    song = getSong('http://www.azlyrics.com/lyrics/taylorswift/blankspace.html', 'test2')
+    words = unique_list(song['song_text'].split())
+    return words
+
+def unique_list(text):
+    ulist = []
+    [ulist.append(x) for x in text if x not in ulist]
+    return ulist
 
 
-    text_file = open( filename +".txt", "w")
-    text_file.write(text)
-    text_file.close()
+for word in get_words():
+    print word
 
 
-getSong('http://www.azlyrics.com/lyrics/faint/totaljob.html', 'test1')
-getSong('http://www.azlyrics.com/lyrics/taylorswift/blankspace.html', 'test2')
-getSong('http://www.azlyrics.com/lyrics/faint/letthepoisonspillfromyourthroat.html', 'test3')
-getSong('http://www.azlyrics.com/lyrics/faint/yourretrocareermelted.html', 'test4')
-getSong('http://www.azlyrics.com/lyrics/faint/posedtodeath.html', 'test5')
+
+# getSong('http://www.azlyrics.com/lyrics/faint/totaljob.html', 'test1')
+# getSong('http://www.azlyrics.com/lyrics/taylorswift/blankspace.html', 'test2')
+# getSong('http://www.azlyrics.com/lyrics/faint/letthepoisonspillfromyourthroat.html', 'test3')
+# getSong('http://www.azlyrics.com/lyrics/faint/yourretrocareermelted.html', 'test4')
+# getSong('http://www.azlyrics.com/lyrics/faint/posedtodeath.html', 'test5')
 
 
+# text_file = open("test11" + ".txt", "w")
+# text_file.write(song['song_text'])
+# text_file.close()
