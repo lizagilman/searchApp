6b18bf7cd2d3af8f9c8f585bf905cc1fd7d305e2
lizag <lizagilman@gmail.com>
2016-10-14 12:25:32 +0300

    get document containign words api return json

--- app/serializers/document_serializer.py
+++
@@ -1,9 +0,0 @@
-from app.models import Document
-from rest_framework import serializers
-from app.serializers.word_str_serializer import WordStrSerializer
-
-
-class DocumentSerializer(serializers.ModelSerializer):
-    words = WordStrSerializer(many=True)
-    class Meta:
-        model = Document
