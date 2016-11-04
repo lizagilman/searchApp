345fac030d885283630a39c5b4374a2d34c7d881
lizag <lizagilman@gmail.com>
2016-11-04 22:40:57 +0200

    gui

--- searchApp_static/src/js/script.js
+++ searchApp_static/src/js/script.js
@@ -7,11 +7,17 @@ app.controller('myCtrl', function ($scope,$http) {
     $scope.searchStr = "";
     $scope.searchResult = [];
     $scope.urlToAdd = "";
-    $scope.search = function () {
+    $scope.loading = false;
+
+    $scope.search = function (query) {
+        $scope.loading=true;
+        $('#resultsHeadline').html("Search results for: "+query);
         $http.get("../search_query/?query="+$scope.searchStr).success(function (data) {
+
             console.log(data);
+            $scope.loading=false;
             $scope.searchResult = [];
-           var searchResults = JSON.parse(data);
+            var searchResults = JSON.parse(data);
             console.log(searchResults);
             angular.forEach(searchResults, function (searchResult) {
                 console.log(searchResult);
@@ -20,10 +26,32 @@ app.controller('myCtrl', function ($scope,$http) {
         });
     };
 
-    $scope.addUrlToDb = function(){
-          $http.get("../add_new_doc/?url="+$scope.urlToAdd).success(function (data) {
-              console.log(data);
-          });
+    $scope.addUrlToDb = function(url){
+        $scope.loading = true;
+        $http.get("../add_new_doc/?url="+url).success(function (data) {
+            console.log(data);
+            if(data=="success"){
+                alert("Saved new song");
+            } else {
+                alert("Failed to save new song");
+            }
+            $scope.loading = false;
+        });
+    };
+
+
+    $scope.go = function(){
+        var searchQuery = $('#searchInput').val();
+        var songUrlToAdd = $('#addSongInput').val();
+        if(searchQuery){
+            $('#searchInput').val("")
+            $scope.search(searchQuery);
+        } else if(songUrlToAdd) {
+            $('#addSongInput').val("")
+            $scope.addUrlToDb(songUrlToAdd);
+        } else {
+            alert("Please insert input");
+        }
     };
 
     console.log($scope.searchResult);
