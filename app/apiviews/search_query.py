dcba87600731e6ef4f5ca31947365bc4661ca9c9
DimaGirya <DimaGirya1990@gmail.com>
2016-11-05 12:54:10 +0200

    Conitnue work

--- app/apiviews/search_query.py
+++ app/apiviews/search_query.py
@@ -12,13 +12,11 @@ class SearchQuery(APIView):
         search_query_array = query.split(' ')
         query_number_of_words = len(search_query_array)
         result = []
+        bracketsResult = []
         if (query_number_of_words == 1):
             search = find_one_word(query)
             result = json.dumps(search)
         else:
-            search_task = []
-            index = 0
-            index_task = 0
             print "search_query_array:"
             print search_query_array
 
@@ -40,61 +38,85 @@ class SearchQuery(APIView):
                     closeBrackets.insert(count, closeBracketsIndex)
                     closeBracketsIndex += 1
                     count += 1
-
-            print openBrackets
             print closeBrackets
-
-            while(index < len(search_query_array)):
-                print search_query_array[index]
-                if(index == 0):
-                    if(search_query_array[index + 1] == "OR"):
-                        search_task.insert(index_task,search_query_array[index] + " OR " + search_query_array[index + 2])
-                        index += 3
-                    elif(search_query_array[index + 1] == "NOT"):
-                        search_task.insert(index_task,search_query_array[index] + " NOT " + search_query_array[index + 2])
-                        index += 3
-                    elif(search_query_array[index + 1] == "AND"):
-                        search_task.insert(index_task,search_query_array[index] + " AND " + search_query_array[index + 2])
-                        index += 3
-                    else:
-                        search_task.insert(index_task,search_query_array[index] + " OR " + search_query_array[index + 1])
-                        index += 2
-                else:
-                    if (search_query_array[index] == "OR"):
-                        search_task.insert(index_task,"# OR " + search_query_array[index + 1])
-                        index += 2
-                    elif (search_query_array[index] == "NOT"):
-                        search_task.insert(index_task,"# NOT " + search_query_array[index + 1])
-                        index += 2
-                    elif (search_query_array[index] == "AND"):
-                        search_task.insert(index_task,"# AND " + search_query_array[index + 1])
-                        index += 2
-                    else:
-                        search_task.insert(index_task,"# OR " + search_query_array[index])
+            print openBrackets
+            if(len(closeBrackets) == len(openBrackets)):
+                    index = 0
+                    while(index < len(openBrackets)):
+                        indexOpenBrackets = openBrackets[index]
+                        indexCloseBrackets = closeBrackets[index]
+                        subQuery = query[indexOpenBrackets + 1:indexCloseBrackets]
+                        search_sub_query = subQuery.split(' ')
+                        search_sub_query = clear_list_from_space(search_sub_query)
+                        bracketsResult.insert(index,search_query(search_sub_query))
                         index += 1
-                index_task += 1
-                print search_task
-            index = 0
-            print "searching...."
-            for search_step in search_task:
-                search = search_step.split(' ')
-                print search
-                if(search[0] == "#"):
-                    if(search[1] == "OR"):
-                       result =  find_or_words(result,find_one_word(search[2]))
-                    elif(search[1] == "AND"):
-                       result = find_and_words(result, find_one_word(search[2]))
-                    elif(search[1] == "NOT"):
-                       result = find_not_words(result, find_one_word(search[2]))
-                else:
-                    if (search[1] == "OR"):
-                        result = find_or_words(find_one_word(search[0]), find_one_word(search[2]))
-                    elif (search[1] == "AND"):
-                        result = find_and_words(find_one_word(search[0]), find_one_word(search[2]))
-                    elif (search[1] == "NOT"):
-                        result = find_not_words(find_one_word(search[0]), find_one_word(search[2]))
-                print result
+                    index = 0
+                    result = search_query(search_query_array)
+            else:
+                result = "SEARCH_SYNTAX_ERROR"
+            print result
             result = json.dumps(result)
         return Response(result)
 
 
+def search_query(search_query_array):
+    result = []
+    search_task = []
+    index = 0
+    index_task = 0
+    while (index < len(search_query_array)):
+        print search_query_array[index]
+        if (index == 0):
+            if (search_query_array[index + 1] == "OR"):
+                search_task.insert(index_task, search_query_array[index] + " OR " + search_query_array[index + 2])
+                index += 3
+            elif (search_query_array[index + 1] == "NOT"):
+                search_task.insert(index_task, search_query_array[index] + " NOT " + search_query_array[index + 2])
+                index += 3
+            elif (search_query_array[index + 1] == "AND"):
+                search_task.insert(index_task, search_query_array[index] + " AND " + search_query_array[index + 2])
+                index += 3
+            else:
+                search_task.insert(index_task, search_query_array[index] + " OR " + search_query_array[index + 1])
+                index += 2
+        else:
+            if (search_query_array[index] == "OR"):
+                search_task.insert(index_task, "# OR " + search_query_array[index + 1])
+                index += 2
+            elif (search_query_array[index] == "NOT"):
+                search_task.insert(index_task, "# NOT " + search_query_array[index + 1])
+                index += 2
+            elif (search_query_array[index] == "AND"):
+                search_task.insert(index_task, "# AND " + search_query_array[index + 1])
+                index += 2
+            else:
+                search_task.insert(index_task, "# OR " + search_query_array[index])
+                index += 1
+        index_task += 1
+        print search_task
+    index = 0
+    print "searching...."
+    for search_step in search_task:
+        search = search_step.split(' ')
+        print search
+        if (search[0] == "#"):
+            if (search[1] == "OR"):
+                result = find_or_words(result, find_one_word(search[2]))
+            elif (search[1] == "AND"):
+                result = find_and_words(result, find_one_word(search[2]))
+            elif (search[1] == "NOT"):
+                result = find_not_words(result, find_one_word(search[2]))
+        else:
+            if (search[1] == "OR"):
+                result = find_or_words(find_one_word(search[0]), find_one_word(search[2]))
+            elif (search[1] == "AND"):
+                result = find_and_words(find_one_word(search[0]), find_one_word(search[2]))
+            elif (search[1] == "NOT"):
+                result = find_not_words(find_one_word(search[0]), find_one_word(search[2]))
+    return  result
+
+def clear_list_from_space(list):
+    for node in list:
+        if(node == ''):
+            list.remove(node)
+    return list
